b8acb7c37d602e0e26141db82565478104b2a94e
hyeonwu <tkanwd11@gmail.com>
2020-08-02 02:00:13 +0900

    Test 코드 적용 , WebTestClient put 매핑오류 확인하기

---
+++ src/test/java/com/example/demo/APITest/MockTest/MemberApiMockTest.java
@@ -0,0 +1,129 @@
+package com.example.demo.APITest.MockTest;
+
+import com.example.demo.member.domain.Address;
+import com.example.demo.member.domain.Member;
+import com.example.demo.member.dto.MemberUpdateRequestDto;
+import com.example.demo.member.service.MemberService;
+import org.hibernate.annotations.LazyToOne;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+/**
+ * 참고!
+ */
+//https://cheolhojung.github.io/posts/java/springboot-test.html
+
+@RunWith(SpringRunner.class)
+//@restController
+//@WebMvcTest(MemberApiController.class) //슬라이싱 테스트. 각레이어별로 빈이 등록된다. Service,Repository 등록이 안된다
+//@WebMvcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient //webTestClient 빈을 만들어준다!
+//@AutoConfigureMockMvc //McokMvc라는 클라이언트를 사용한다 -->
+public class MemberApiMockTest {
+
+
+    /**
+     * 서블릿기반에 웹MVC가아닌 비동기Call을 하는 web-flux
+     * 아래의 WebTestClient는 논블럭킹 + 비동기 call을 지원한다. restTemplate의 대체자로 불린다.
+     */
+    @Autowired
+    WebTestClient webTestClient;
+
+    @LocalServerPort
+    private int port;
+
+    /**
+     * Mocking : MemberService
+     * 해당 빈을 MockBean을 교체한다. memberService를 사용하는 컨트롤러는 mocking된 아래의 memberService를 사용한다.
+     * 즉 아래의memberService는 원본이 아니다
+     * 프로젝트 전체를 빌드하면서 컴포넌트스캔을 해주고 아래의 mockbean으로 설정한 memberService만 교체해준다.
+     */
+
+    @Autowired
+    MemberService memberService;
+
+    /**
+     * @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+     * 서블릿컨테이너를 Mocking 한다 DispatchServlet 에다 요청을 보내는것과 비스무리하게
+     * 실험을 한다. 목업이 된 서블릿에 어떠한 인터렉션을 하려면 목mvc라는 클라이언트를 꼭 사용해야한다.
+     *
+     */
+
+    /**
+     * 실제로 서블릿이뜬다. MOCKMVC가 아닌 테스트용Restemplate, webclient를 적용해야한다.
+     * 겹치지않는 랜덤한 포트에 뜬다.
+     */
+//    @Autowired
+//    private MockMvc mockMvc; //주입받기
+
+
+    @PersistenceContext
+    EntityManager em;
+
+
+    @Test
+    public void MockingWebFlux() throws Exception {
+        when(memberService.mockTest()).thenReturn("hyunwoo");
+        webTestClient.get().uri("/mockTest").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hyunwoo");
+    }
+
+
+    @Test
+    public void 회원수정WebFlux() throws Exception {
+
+        Address address = new Address("kk", "kfc", "def");
+        MemberUpdateRequestDto requestDto = MemberUpdateRequestDto.builder()
+                .city(address.getCity())
+                .zipcode(address.getZipcode())
+                .street(address.getStreet())
+                .build();
+
+        //        when(memberService.update(2L, requestDto)).thenReturn(2L); //mocking --> @mockBean
+
+        String url = "http://localhost:" + port + "/api/member/delete/2";
+
+        Member member = em.find(Member.class, 2L);
+        webTestClient.put().uri(url)
+//                .body(requestDto, MemberUpdateRequestDto.class)
+                .exchange()
+                .expectStatus().isNoContent()
+                .expectBody().isEmpty();
+    }
+
+
+    @Test(expected = AssertionError.class)
+    public void 회원삭제WebFlux() throws Exception {
+
+        Member member = em.find(Member.class, 97L);
+        memberService.delete(member.getId());
+        String url = "http://localhost:" + port + "/api/member/delete/" + member.getId();
+
+        webTestClient.delete().uri(url)
+                .exchange()
+                .expectStatus().isOk();
+//        restTemplate.delete(url);
+        em.find(Member.class, 97L);
+
+        fail("사용자 없음"); //expected 제외할 경우 해당 문구가 뜬다.
+    }
+
+}
