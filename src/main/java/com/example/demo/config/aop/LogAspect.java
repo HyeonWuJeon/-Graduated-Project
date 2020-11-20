f2637c3c3c3c4185cd75b509aa56db7c010190a4
HyeonWuJeon <tkawnd111@gmail.com>
2020-11-21 01:39:43 +0900

    주석 추가

--- src/main/java/com/example/demo/config/aop/LogAspect.java
+++ src/main/java/com/example/demo/config/aop/LogAspect.java
@@ -18,11 +18,11 @@ public class LogAspect {
     @Around("@annotation(com.example.demo.config.aop.LogExecutionTime)") //해당어노테이션 주변에다가 밑의코드를 적용한다.
     //around 어노테이션을 사용함으로써 joinPoint를 받을 수있다. joinpoint -> 애노테이션이 붙여져있는 메소드가 들어옴
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
-        StopWatch stopWatch = new StopWatch();
+        StopWatch stopWatch = new StopWatch(); //실행시간 측정
         stopWatch.start();
         Object proceed = joinPoint.proceed(); //조인포인트를 실행하겠다.
         stopWatch.stop();
-        logger.info("관리자 조회 서비스 실행시간 측정"+ stopWatch.prettyPrint());
+        logger.info("실행시간 측정 : "+ stopWatch.prettyPrint());
         return proceed; //결과를 리턴하겠다.
     }
 
