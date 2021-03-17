6177540f66968dcc5f4bd58aeb5530c142a99f8a
HyeonWuJeon <tkawnd111@gmail.com>
2021-03-18 06:58:01 +0900

    TEST

--- scripts/start.sh
+++ scripts/start.sh
@@ -34,4 +34,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
